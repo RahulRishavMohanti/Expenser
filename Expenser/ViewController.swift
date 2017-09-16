c4c74de2bc729393e2af51a8bd82fc9f55d5b40d
Rahul Mohanti <jarvis3.0@Rahuls-MacBook-Air.local>
2017-09-17 00:18:12 +0530

    Initial Commit

---
+++ Expenser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Expenser
+//
+//  Created by Rahul Rishav Mohanti on 17/09/17.
+//  Copyright © 2017 Rahul Rishav Mohanti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
