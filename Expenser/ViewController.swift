9e4aff258e38d0d5b1a882a173ba8b98f0db54a0
Rahul Rishav Mohanti <jarvis3.0@Rahuls-MacBook-Air.local>
2017-10-07 21:45:18 +0530

    pods added

--- Expenser/ViewController.swift
+++ Expenser/ViewController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+import Canvas
+class ViewController: UIViewController, UITextFieldDelegate {
+    var x=""
+    @IBOutlet weak var checkInView: CSAnimationView!
+    @IBOutlet weak var ScrollView: UIScrollView!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var Password: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +27,80 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder();
+    
+        return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+          if(textField == Password && textField.isFirstResponder)
+          {
+          ScrollView.setContentOffset(CGPoint(x: 0, y: 250), animated: true)
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+         ScrollView.setContentOffset(CGPoint(x: 0, y: 0), animated:true)
+        
+    }
+    
+    
+    @IBAction func checkIn(_ sender: Any)
+    {
+        checkInView.type=CSAnimationTypeZoomOut
+        checkInView.startCanvasAnimation()
+        print("clicked")
+        Password.resignFirstResponder()
+        Auth.auth().createUser(withEmail: username.text!, password: Password.text!)
+        { (user, error) in
+            if error != nil
+            {
+                self.login()
+            }
+            else
+            {
+                print("User Created")
+                self.login()
+            }
+        }
 
-
+    }
+    func login(){
+        Auth.auth().signIn(withEmail: username.text!, password: Password.text!) { (user, error) in
+            if error != nil
+            {   self.checkInView.type = CSAnimationTypeShake
+                self.checkInView.duration = 1
+                self.checkInView.startCanvasAnimation()
+                print("Incorrect Pass")
+            }
+            else
+            {   //print (user?.email ?? "rahul")
+                var ref: DatabaseReference!
+                ref = Database.database().reference()
+                ref.updateChildValues(["currentUser":user?.uid ?? "none"])
+                
+                 self.performSegue(withIdentifier: "firstSegue", sender: nil)
+                ref.child("NameFromUID").observeSingleEvent(of: .value, with: { (snapshot) in
+                    // Get user value
+                    let value = snapshot.value as? NSDictionary
+                    print(value)
+                    self.x = value?[(user?.uid)!] as? String ?? ""
+                    print (self.x)
+                    print ("Huzzah")
+                    
+                    self.performSegue(withIdentifier: "firstSegue", sender: nil)
+                    
+                    
+                })
+                { (error) in
+                    print(error.localizedDescription)
+                }
+                
+            }
+        }
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Expenser' do
+    pod 'Canvas'
+    pod 'Firebase/Core'
+    pod 'Firebase/Auth'
+    pod 'Firebase/Database'
+    use_frameworks!
+    pod "TextFieldEffects"
+end
